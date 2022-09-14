c6347cf5807ee53ae04674fd71cc1a16be1a7a58
= <=>
2022-09-14 13:32:59 -0300

    feat: add the Answer model and an empty spec file

---
+++ model/answer.spect.ts
@@ -0,0 +1,1 @@
+import { AnswerModel } from "./answer";
---
+++ model/answer.ts
@@ -0,0 +1,51 @@
+type InputProps = {
+  value: string;
+  isCorrect?: boolean;
+  isRevealed?: boolean;
+};
+
+type OutputProps = {
+  value: string;
+  isCorrect: boolean;
+  isRevealed: boolean;
+};
+
+export class AnswerModel {
+  #value: string;
+  #isCorrect: boolean;
+  #isRevealed: boolean; // ui state?
+
+  constructor(props: InputProps) {
+    this.#value = props.value;
+    this.#isCorrect = props.isCorrect || false;
+    this.#isRevealed = props.isRevealed || false;
+  }
+
+  static correct(value: string) {
+    return new AnswerModel({ value, isCorrect: true });
+  }
+
+  static incorrect(value: string) {
+    return new AnswerModel({ value, isCorrect: false });
+  }
+
+  get isCorrect() {
+    return this.#isCorrect;
+  }
+
+  get isRevealed() {
+    return this.#isRevealed;
+  }
+
+  reveal(): AnswerModel {
+    return new AnswerModel({ ...this.toJSON(), isRevealed: true });
+  }
+
+  toJSON(): OutputProps {
+    return {
+      value: this.#value,
+      isCorrect: this.#isCorrect,
+      isRevealed: this.#isRevealed,
+    };
+  }
+}
